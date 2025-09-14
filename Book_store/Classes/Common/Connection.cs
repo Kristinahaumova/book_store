86c52c16a6c4257561973ab9f0cf9d5b8df5a5e2
Kristinahaumova <kristinahaumova@gmail.com>
2025-09-14 19:27:12 +0500

    Создала класс Connection. Хаюмова

--- Book_store/Classes/Common/Connection.cs
+++ Book_store/Classes/Common/Connection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace Book_store.Classes.Common
 {
     public class Connection
     {
+        readonly static string path = "server=localhost;port=3306;database=book_store;uid=root;";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(path);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(SQL, connection);
+            return command.ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
     }
 }
