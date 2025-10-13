5d00c813ad63d18f7364f41197e3e5ef5f727e5c
Kristinahaumova <kristinahaumova@gmail.com>
2025-10-14 00:06:50 +0500

    Сделала поиск по названиям и авторам. Хаюмова

--- Book_store/Pages/UserMainMenu.xaml.cs
+++ Book_store/Pages/UserMainMenu.xaml.cs
@@ -3,7 +3,9 @@ using Book_store.Elements;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,18 +23,83 @@ namespace Book_store.Pages
     /// <summary>
     /// Логика взаимодействия для UserMainMenu.xaml
     /// </summary>
-    public partial class UserMainMenu : Page
+    public partial class UserMainMenu : Page, INotifyPropertyChanged
     {
         private UserContext currentUser;
+        private ObservableCollection<BookContext> _allBooks;
+        private CollectionViewSource _filteredBooksView;
+        private string _searchText = string.Empty;
+
+        public ObservableCollection<BookContext> AllBooks
+        {
+            get => _allBooks;
+            set
+            {
+                _allBooks = value;
+                OnPropertyChanged();
+                ApplyFilter();
+            }
+        }
+
+        public CollectionViewSource FilteredBooksView
+        {
+            get => _filteredBooksView;
+            set
+            {
+                _filteredBooksView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+                ApplyFilter();
+            }
+        }
+
         public UserMainMenu(UserContext user)
         {
             InitializeComponent();
             currentUser = user;
+            DataContext = this;
 
-            var allBooks = new BookContext().AllBooks();
-            DataContext = new { Books = new ObservableCollection<BookContext>(allBooks) };
+            LoadBooks();
         }
 
+        private void LoadBooks()
+        {
+            var allBooks = new BookContext().AllBooks();
+            AllBooks = new ObservableCollection<BookContext>(allBooks);
+
+            FilteredBooksView = new CollectionViewSource { Source = AllBooks };
+            FilteredBooksView.Filter += Books_Filter;
+            ApplyFilter();
+        }
+
+        private void Books_Filter(object sender, FilterEventArgs e)
+        {
+            if (e.Item is BookContext book)
+            {
+                bool searchMatch = string.IsNullOrEmpty(SearchText) ||
+                                   book.Name.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                                   book.AuthorName.IndexOf(SearchText, StringComparison.OrdinalIgnoreCase) >= 0;
+
+                e.Accepted = searchMatch;
+            }
+        }
+
+        private void ApplyFilter()
+        {
+            if (FilteredBooksView?.View != null)
+            {
+                FilteredBooksView.View.Refresh();
+            }
+        }
 
         private void Order(object sender, RoutedEventArgs e)
         {
@@ -44,6 +111,12 @@ namespace Book_store.Pages
             MainWindow.init.CurrentUser = null;
             MainWindow.init.CartItems.Clear();
             MainWindow.init.OpenPage(new Authorization());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
